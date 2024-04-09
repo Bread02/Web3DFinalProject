dffa10afcffa33c6f7312bf89df3658bb0e3a4ae
Bread02 <79101834+Bread02@users.noreply.github.com>
2024-04-09 11:38:53 +0100

    Added new fonts and .js and .css

---
+++ assignment/application/controller/controller.php
@@ -0,0 +1,25 @@
+<?php
+class Controller {
+
+	// Declare public variables for the controller class
+	public $load;
+	public $model;
+
+    function __construct($pageURI = null)
+    {
+
+        $this->load = new Load();
+        $this->model = new Model();
+        // What page we are on
+        $this->$pageURI();
+    }
+
+
+    function home()
+	{
+        $data = $this->model->model3D_info();
+        $this->load->view('home', $data);
+	}
+}
+
+?>
---
+++ assignment/application/model/model.php
@@ -0,0 +1,48 @@
+<?php   
+class Model {
+	
+	// edit for coursework
+	    public function __construct()
+    {
+        $dsn = 'sqlite:./db/test1.db';
+
+        try {
+            $this->dbhandle = new PDO($dsn, 'user', 'password', array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            )
+            );
+        } catch (PDOEXception $e) {
+            echo "I'm sorry, Martin. I'm afraid I can't connect to the database!";
+            // Generate an error message if connection fails
+            print new Exception($e->getMessage());
+        }
+    }
+	
+   // method to simulate the model data
+    public function model3D_info()
+    {
+    // simulate the model's data
+        return array(
+            'model_1' => 'Coke Can 3D Image 1',
+            'image3D_1' => 'coke_1',
+
+            'model_2' => 'Coke Can 3D Image 2',
+            'image3D_2' => 'coke_2',
+
+            'model_3' => 'Sprite Bottle 3D Image 1',
+            'image3D_3' => 'sprite_1',
+
+            'model_4' => 'Sprite Bottle 3D Image 2',
+            'image3D_4' => 'sprite_2',
+
+            'model_5' => 'Dr Pepper Cup 3D Image 1',
+            'image3D_5' => 'pepper_1',
+
+            'model_6' => 'Dr Pepper Cup 3D Image 2',
+            'image3D_6' => 'pepper_2',
+
+        );
+    }
+}
+?>
