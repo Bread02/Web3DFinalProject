32fa5d5cf730a3fa46499f9f347e3647f8856907
Bread02 <79101834+Bread02@users.noreply.github.com>
2024-05-06 16:09:31 +0100

    Fixed pages

--- assignment/application/js/swap_restyle.js
+++ assignment/application/js/swap_restyle.js
@@ -1,61 +1,171 @@
-var counter = 0
+// This script controls the pages that are accessed by pressing buttons.
+// It reads the id of the page, for example a id="originality", then shows/hides that.
 
-function swap(selected) 
-{
-    // <!-- Hide all elements -->
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('models').style.display = 'none';
-    document.getElementById('originality').style.display = 'none';
-    document.getElementById('acknowledgements').style.display = 'none';
-    document.getElementById('references').style.display = 'none';
-
-    // only display selected element
-    document.getElementById(selected).style.display = 'block';
-}
-
+var counter = 0;
 $(document).ready(function() {
 	
 	selectPage();
+	selectModel();
 
 	function selectPage() {
 
 		$('#home').show();
 		$('#about').hide();
 		$('#models').hide();
-	//	$('#interaction').hide();
-	//	$('#cokeDescription').hide();
-	//	$('#spriteDescription').hide(); 
-	//	$('#pepperDescription').hide(); 
+		$('#interaction').hide();
+		$('#originality').hide();
+		$('#acknowledgements').hide();
+		$('#references').hide();
+
+		$('#cokeDescription').hide();
+		$('#spriteDescription').hide(); 
+		$('#pepperDescription').hide(); 
+
 
 		$('#navHome').click(function(){
 			$('#home').show();
 			$('#about').hide();
 			$('#models').hide();
-	//		$('#interaction').hide();
-		//	$('#cokeDescription').hide();
-		//	$('#spriteDescription').hide(); 
-		//	$('#pepperDescription').hide(); 	  
+			$('#interaction').hide();
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+			$('#originality').hide();
+			$('#acknowledgements').hide();
+			$('#references').hide();			
 		});
 
 		$('#navAbout').click(function(){
 			$('#home').hide();
 			$('#about').show();
 			$('#models').hide();
-		//	$('#interaction').hide();
-		//	$('#cokeDescription').hide();
-		//	$('#spriteDescription').hide(); 
-		//	$('#pepperDescription').hide(); 	  
+			$('#interaction').hide();
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+			$('#originality').hide();
+			$('#acknowledgements').hide();
+			$('#references').hide();			
 		});
 
 		$('#navModels').click(function(){
 			$('#home').hide();
 			$('#about').hide();
 			$('#models').show();
-		//	$('#interaction').show(); 
-		//	$('#cokeDescription').show();
-		//	$('#spriteDescription').hide(); 
-		//	$('#pepperDescription').hide(); 
+			$('#interaction').show(); 
+			$('#cokeDescription').show();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+			$('#originality').hide();
+			$('#acknowledgements').hide();
+			$('#references').hide();
+		});
+		
+		$('#originality').click(function(){
+			$('#home').hide();
+			$('#about').hide();
+			$('#models').hide();
+			$('#interaction').hide(); 
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+			$('#originality').show();
+			$('#acknowledgements').hide();
+			$('#references').hide();
+		});
+		
+		$('#acknowledgements').click(function(){
+			$('#home').hide();
+			$('#about').hide();
+			$('#models').show();
+			$('#interaction').hide(); 
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+			$('#originality').hide();
+			$('#acknowledgements').show();
+			$('#references').hide();
+		});
+		
+		$('#references').click(function(){
+			$('#home').hide();
+			$('#about').hide();
+			$('#models').hide();
+			$('#interaction').hide(); 
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+			$('#originality').hide();
+			$('#acknowledgements').hide();
+			$('#references').show();
 		});
 	}
-  
+
+	function selectModel() {
+
+		$('#navCoke').click(function(){
+			$('#coke').show();
+			$('#sprite').hide();
+			$('#pepper').hide();
+			$('#interaction').show(); 
+			$('#cokeDescription').show();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+		});
+
+		$('#navSprite').click(function(){
+			$('#coke').hide();
+			$('#sprite').show();
+			$('#pepper').hide();
+			$('#interaction').show(); 
+			$('#cokeDescription').hide();
+			$('#spriteDescription').show();
+			$('#pepperDescription').hide();  	  	  
+		});
+
+		$('#navPepper').click(function(){
+			$('#coke').hide();
+			$('#sprite').hide();
+			$('#pepper').show();
+			$('#interaction').show(); 
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').show(); 	   
+		});
+	}
+
 });
+
+function changeLook() {
+	counter += 1;
+	switch (counter) {
+		case 1:
+			document.getElementById('bodyColor').style.backgroundColor = 'lightblue';
+			document.getElementById('headerColor').style.backgroundColor = '#FF0000';
+			document.getElementById('footerColor').style.backgroundColor = '#FF9900';
+			break;
+		case 2:
+			document.getElementById('bodyColor').style.backgroundColor = '#FF6600';
+			document.getElementById('headerClor').style.backgroundColor = '#FF9999';
+			document.getElementById('footerColor').style.backgroundColor = '#996699';
+			break;
+		case 3:
+			document.getElementById('bodyColor').style.backgroundColor = 'coral';
+			document.getElementById('headerColor').style.backgroundColor = 'darkcyan';
+			document.getElementById('footerColor').style.backgroundColor = 'darksalmom';
+			break;
+		case 4:
+			counter = 0;
+			document.getElementById('bodyColor').style.backgroundColor = 'lightgrey';
+			document.getElementById('headerColor').style.backgroundColor = 'chocolate';
+			document.getElementById('footerColor').style.backgroundColor = 'dimgrey';
+			break;
+	}
+}
+
+function changeBack() {
+	document.getElementById('bodyColor').style.backgroundColor = '#FFFFFF';
+	document.getElementById('headerColor').style.backgroundColor = 'rgba(175,0,0,1)';
+	document.getElementById('footerColor').style.backgroundColor = 'rgba(175,0,0,1)';
+}
+
--- assignment/application/view/home.php
+++ assignment/application/view/home.php
@@ -23,17 +23,16 @@
 
 <body>
 
-<!-- START OF NAVBAR -->
+  <!-- START OF NAVBAR -->
   <nav class="navbar sticky-top navbar-expand-sm navbar_coca_cola">
-
-  <!-- START OF LOGO -->
-  <div class="logo">
-    <div class="container-fluid">
-      <!-- Logo -->
-      <h1>Costa Coffee World</h1>
+    <!-- START OF LOGO -->
+    <div class="logo">
+      <div class="container-fluid">
+        <!-- Logo -->
+        <h1>Costa Coffee World</h1>
+      </div>
     </div>
-</div>
-  <!-- END OF LOGO -->
+    <!-- END OF LOGO -->
 
     <!-- START OF NAVIGATION ELEMENT OF BAR -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
@@ -46,15 +45,15 @@
         <!-- Links for navbar -->
         <ul class="navbar-nav mr-auto"> <!-- mx-auto = centre align, default to left, mr-auto to right -->
           <li class="nav-item">
-            <a class="nav-link active" href="javascript:swap('home')">Home</a>
+            <a class="nav-link active" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-placement="bottom"
+            <a id="navAbout" class="nav-link" href="#" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-placement="bottom"
               title="About Web 3D Applications"
               data-bs-content="3D Apps is a final year and postgraduate module ...">About</a>
           </li>
           <li class="nav-item">
-            <a id="navModels" class="nav-link" href="javascript:swap('models')" data-bs-toggle="popover"
+            <a id="navModels" class="nav-link" href="#" data-bs-toggle="popover"
               data-bs-trigger="hover" data-placement="bottom" title="X3D Models"
               data-content="There are three X3D models: Coke, Sprite and Pepper">Models</a>
           </li>
@@ -66,15 +65,15 @@
         </ul>
       </div>
     </div>
-    </div>
     <!-- END OF NAVIGATION ELEMENT OF BAR -->
 
   </nav>
-<!-- END OF TOP NAVBAR -->
+  <!-- END OF TOP NAVBAR -->
 
   <!-- Start of the SPA -->
   <!-- START OF HOME -->
   <div id="home">
+    <!-- START OF FIRST ROW -->
     <div class="col-sm-12">
       <div id="main_3d_image">
         <div id="main_text" class="col-xs-12">
@@ -84,7 +83,9 @@
         </div>
       </div>
     </div>
+    <!-- END OF FIRST ROW -->
 
+    <!-- START OF SECOND ROW -->
     <div class="row">
       <div class="col-sm-12">
       </div>
@@ -92,21 +93,28 @@
     <br>
     <div class="row">
       <div class="col-sm-4">
-        <img src="assets/images/front_page_images/FrontImage2.png" alt="Image of Costa Coffee Cup">
-        <br>Costa coffee
+        <div class="card-body">
+          <img src="assets/images/front_page_images/FrontImage2.png" alt="Image of Costa Coffee Cup">
+          <h4 class="card-title"> Coffee In Store</h4>
+          <p class="card-text">In store you can buy coffee.</p>
+        </div>
       </div>
       <div class="col-sm-4">
-      <img src="assets/images/front_page_images/FrontImage3.png" alt="Image of Costa Coffee Milkshake">
-        Costa Milkshake
+        <div class="card-body">
+          <img src="assets/images/front_page_images/FrontImage3.png" alt="Image of Costa Coffee Milkshake">
+          <h4 class="card-title">Costa Frappe Bottle</h4>
+        </div>
       </div>
       <div class="col-sm-4">
-      <img src="assets/images/front_page_images/FrontImage4.png" alt="Image of Costa Coffee Canned">
-        Costa Canned
+        <div class="card-body">
+          <img src="assets/images/front_page_images/FrontImage4.png" alt="Image of Costa Coffee Canned">
+          <h4 class="card-title">Costa in a Can</h4>
+        </div>
       </div>
     </div>
   </div>
 
-<!-- END OF HOME -->
+  <!-- END OF HOME -->
 
   <!-- START OF MODELS -->
   <div id="models" class="container-fluid main_contents">
@@ -114,86 +122,209 @@
       <!-- The 3D model holder -->
       <div class="col-sm-9">
         <div class="card text-left">
-      </div>
-      <!-- Where the actual 3D models are held -->
-      <div class="card-body">
-      <h4 class="card-title"> Coca Cola X3D Model</h4>
-        <div class="model3D">
-          <x3d>
-            <scene>
-              <inline url="assets/x3d/coke.x3d"></inline>
-            </scene>
-          </x3d>
+        </div>
+
+        <!-- MODEL 1 -->
+        <div id="coke">
+        <!-- The 3D Model Card -->
+        <div class="card-body">
+          <h4 class="card-title"> Coca Cola X3D Model</h4>
+          <!-- TODO Nav Menu to choose models -->
+          <a id="navCoke" button type="button" class="btn btn-primary">Cup</button> </a>
+          <a id="navSprite" button type="button" class="btn btn-secondary">Bottle</button> </a>
+          <a id="navPepper" button type="button" class="btn btn-success">Can</button> </a>
+          <!-- END OF NAV MENU TO CHOOSE MODELS -->
+          <div class="model3D">
+            <x3d id="model">
+              <scene>
+                <inline namespacename="model" mapDEFToID="true" onclick="animateModel()" url="assets/x3d/coke.x3d" ></inline>
+              </scene>
+            </x3d>
+          </div>
         </div>
       </div>
+      <!-- END OF MODEL 1 -->
+
+      <!-- MODEL 2 -->
+      <div id="sprite" style="display:none;">
+        <!-- The 3D Model Card -->
+        <div class="card-body">
+          <h4 class="card-title"> Sprite X3D Model</h4>
+          <!-- TODO Nav Menu to choose models -->
+          <button type="button" class="btn btn-primary">Cup</button>
+          <button type="button" class="btn btn-secondary">Bottle</button>
+          <button type="button" class="btn btn-success">Can</button>
+          <!-- END OF NAV MENU TO CHOOSE MODELS -->
+          <div class="model3D">
+            <x3d id="model">
+              <scene>
+                <inline namespacename="model" mapDEFToID="true" onclick="animateModel()" url="assets/x3d/sprite.x3d" ></inline>
+              </scene>
+            </x3d>
+          </div>
+        </div>
       </div>
-       <!-- Where the gallery is held -->
-       <div class="col-sm-3">
-  <div class="card text-left">
-    <div class="card-header gallery-header">
-      Gallery
-    </div>
-    <div class="card-body">
-      <h4 class="card-title"> 3D Image Gallery</h4>
-      <!-- Dynamically genmerated image gallery using JS and PHP -->
-      <div id="gallery_coke" class="gallery"></div>
-      <p class="card-text">These 3D images of the Coke can, Sprite bottle and Dr Pepper cup could be rendered</p>
-    </div>
-  </div>
-</div>
+      <!-- END OF MODEL 2-->
+
+      <!-- MODEL 3 -->
+      <div id="pepper" style="display:none;">
+        <!-- The 3D Model Card -->
+        <div class="card-body">
+          <h4 class="card-title"> Pepper X3D Model</h4>
+          <!-- TODO Nav Menu to choose models -->
+          <button type="button" class="btn btn-primary">Cup</button>
+          <button type="button" class="btn btn-secondary">Bottle</button>
+          <button type="button" class="btn btn-success">Can</button>
+          <!-- END OF NAV MENU TO CHOOSE MODELS -->
+          <div class="model3D">
+            <x3d id="model">
+              <scene>
+                <inline namespacename="model" mapDEFToID="true" onclick="animateModel()" url="assets/x3d/pepper.x3d" ></inline>
+              </scene>
+            </x3d>
+          </div>
+        </div>
+      </div>
+      <!-- END OF MODEL 3 -->
+      </div>
+      <!-- Where the gallery is held -->
+      <div class="col-sm-3">
+        <div class="card text-left">
+          <div class="card-header gallery-header">
+            Gallery
+          </div>
+          <div class="card-body">
+            <h4 class="card-title"> 3D Image Gallery</h4>
+            <!-- Image gallery using JS and PHP -->
+            <div id="gallery_coke" class="gallery"></div>
+            <p class="card-text">These 3D images have been rendered</p>
+          </div>
+        </div>
       </div>
     </div>
+    <br>
+
+    <div class="row"> <!-- This will hold the model interactions -->
+    <!-- CAMERA VIEWS -->
+      <div class="col-sm-3">
+        <div class="card-body">
+          <h4 class="card-title">Camera Views</h4>
+          <!-- TODO: MAKE THE BELOW BUTTONS FUNCTIONAL -->
+          <!-- The X3D model file will contain the camera locations -->
+          <button type="button" class="btn btn-primary" onclick="cameraFront();">Front</button>
+          <button type="button" class="btn btn-secondary" onclick="cameraBack();">Back</button>
+          <button type="button" class="btn btn-success" onclick="cameraLeft();">Left</button>
+          <button type="button" class="btn btn-success" onclick="cameraRight();">Right</button>
+          <button type="button" class="btn btn-success" onclick="cameraTop();">Top</button>
+          <button type="button" class="btn btn-success" onclick="cameraBottom();">Bottom</button>
+          <br>
+          Use the buttons to manipulate the camera.
+        </div>
+      </div>
+      <!-- END OF CAMERA VIEWS -->
+      <!-- ANIMATION CONTROLS -->
+      <div class="col-sm-3">
+      <div class="card-body">
+      <h4 class="card-title">Animation</h4>
+      <button type="button" class="btn btn-primary" onclick="spin();">RotX</button>
+          <button type="button" class="btn btn-secondary btn-responsive">Bottle</button>
+          <button type="button" class="btn btn-success">Can</button>
+          <br>
+          Use the buttons to animate the camera.
+      </div>
+      </div>
+      <!-- END OF ANIMATION CONTROLS -->
+      <!-- RENDERER CONTROLS -->
+      <div class="col-sm-3">
+      <div class="card-body">
+      <h4 class="card-title">Render</h4>
+      <button type="button" class="btn btn-primary" onclick="wireframe();">Wireframe</button>
+          <button type="button" class="btn btn-secondary">Bottle</button>
+          <button type="button" class="btn btn-success">Can</button>
+          <br>
+          Use the buttons to change the rendering options.
+        </div>
+      </div>
+      <!-- END OF RENDERER CONTROLS -->
+      </div>
+      <!-- END OF ROW -->
+
+      <!-- START OF FINAL ROW -->
+      <br>
+      <div class="row"> <!-- This will hold the model description -->
+        <div class="col-sm-6">
+          <div class="card-body">
+            <h4 class="card-title">Costa In Stores</h4>
+            <p class="card-text">Costa offers a wide range of drinks including Espressos and Americanos</p>
+            <button type="button" class="btn btn-success">Visit the costa store</button>
+          </div>
+        </div>
+      </div>
+      <br>
+    </div>
+<!-- END OF FINAL ROW -->
+
     <!-- END OF MODELS -->
 
-  <!-- START OF STATEMENT OF ORIGINALITY -->
-  <div id="originality">
-    "These web pages are submitted as part requirement for the degree of Computer Science (BSc) at the University of
-    Sussex.
-    They are the product of my own labour except where indicated in the web page content.
-    These web pages or contents may be freely copied and distributed provided the source is acknowledged".
-  </div>
-  <!-- END OF STATEMENT OF ORIGINALITY -->
+    <!-- START OF STATEMENT OF ORIGINALITY -->
+    <div id="originality">
+      "These web pages are submitted as part requirement for the degree of Computer Science (BSc) at the University of
+      Sussex.
+      They are the product of my own labour except where indicated in the web page content.
+      These web pages or contents may be freely copied and distributed provided the source is acknowledged".
+    </div>
+    <!-- END OF STATEMENT OF ORIGINALITY -->
 
 
-  <!-- ACKNOWLEDGEMENTS -->
-  <div id="acknowledgements">
-    Acknowledgements to X and Y.
-  </div>
-  <!-- END OF ACKNOWLEDGEMENTS -->
+    <!-- ACKNOWLEDGEMENTS -->
+    <div id="acknowledgements">
+      Acknowledgements to X and Y.
+</div>
+    <!-- END OF ACKNOWLEDGEMENTS -->
 
     <!-- START OF REFERENCES -->
-  <div id="references">
-    Reference Sources:
-    - Example 1
-    - Example 2
-    - Example 3
-  </div>
+    <div id="references">
+      Reference Sources:
+      - Example 1
+      - Example 2
+      - Example 3
+    </div>
     <!-- END OF REFERENCES -->
 
-  <!-- START OF FOOTER -->
-  <nav class="navbar navbar-expand-sm footer">
-    <div class="container-fluid">
-      <li>
-	  <!-- TODO Put the below styling into CSS rather than here -->
-        <a href="javascript:swap('originality')" style="color:#ffffff;">Statement of Originality</a>
-        <a href="javascript:swap('acknowledgements')" style="color:#ffffff;">Acknowledgements</a>
-        <a href="javascript:swap('references')" style="color:#ffffff;">References</a>
-      </li>
-    </div>
-  </nav>
-  <!-- END OF FOOTER -->
+    <!-- START OF FOOTER -->
+    <!-- TODO: FOOTER BUTTONS NOT WORKING, UNKNOWN CAUSE -->
+    <nav class="navbar navbar-expand-sm footer">
+      <div class="container-fluid">
+      <ul class="navbar-nav mr-auto"> <!-- mx-auto = centre align, default to left, mr-auto to right -->
+        <li class="nav-item">
+          <!-- TODO Put the below styling into CSS rather than here -->
+          <a id="originality" href="#" data-content="There are three X3D models: Coke, Sprite and Pepper" data-placement="bottom" title="X3D Models" style="color:#ffffff;">Statement of Originality |</a>
+          </li>
+          <li class="nav-item">
+          <a id="acknowledgements" href="#" style="color:#ffffff;">Acknowledgements | </a>
+          </li>
+          <li class="nav-item">
+          <a id="references" href="#" style="color:#ffffff;">References | </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- END OF FOOTER -->
 
-  <!-- JavaScript -->
-  <script src="application/js/jquery-1.4.3.min.js" crossorigin="anonymous"></script>
-  <script src="application/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <!-- <script src="application/js/popper.min.js" crossorigin="anonymous"></script> -->
-  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>    -->
-  <script src="application/js/custom.js" crossorigin="anonymous"></script>
-  <script src="application/js/swap_restyle.js"></script>
-  <script type='text/javascript' src='application/js/x3dom.js'></script>
-  <script src="application/js/jquery.fancybox.min.js"></script>
-  <!-- Try to put this in controller if we can? -->
-  <script>javascript: swap('home')</script>
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+     <!-- <script src="application/js/popper.min.js" crossorigin="anonymous"></script> -->
+    <script src="application/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="application/js/all.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="application/js/custom.js" crossorigin="anonymous"></script>
+    <script src="application/js/swap_restyle.js"></script>
+    <script src='application/js/x3dom.js'></script>
+    <script src="application/js/modelInteractions.js"></script>
+    <script src="application/js/getJsonData.js"></script>
+
+    <!-- Try to put this in controller if we can? -->
 
 </body>
+
 </html>
